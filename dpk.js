53521f56337ee4806da45b4f05d159f795cba491
Edmundo Retama <edmret@gmail.com>
2022-12-07 13:25:27 -0600

    finished refactor to make work with new code

--- dpk.js
+++ dpk.js
@@ -1,28 +1,50 @@
 const crypto = require("crypto");
+const {TRIVIAL_PARTITION_KEY, MAX_PARTITION_KEY_LENGTH} = require("./constants");
+
+/**
+ * Extract thje partition key from the event object.
+ * @param {*} event 
+ * @returns {string | Object} the partition key of the event Object
+ */
+function getPartitionKey(event) {
+  if(!event) {
+    return TRIVIAL_PARTITION_KEY;
+  }
+
+  if (event.partitionKey) {
+    return event.partitionKey;
+  }
+  
+  const data = JSON.stringify(event);
+  return crypto.createHash("sha3-512").update(data).digest("hex");
+}
+
+/**
+ * Get partition key as string
+ * @param {string | object} partitionKey 
+ * @returns 
+ */
+function ensurePartitionKeyIsString(partitionKey) {
+  if (typeof partitionKey !== "string") {
+    return JSON.stringify(partitionKey);
+  }
+  return partitionKey;
+}
+
+/**
+ * ensure that the key is no more than 256 chars long
+ * @param {string} partitionKey the partition key
+ * @returns {string} the partition key sized at 256 chars
+ */
+function getCorrectSizedPartitionKey(partitionKey) {
+  if (partitionKey.length > MAX_PARTITION_KEY_LENGTH) {
+    return crypto.createHash("sha3-512").update(partitionKey).digest("hex");
+  }
+  return partitionKey;
+}
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+  const partitionKey = getPartitionKey(event);
+  const partitionKeyAsString = ensurePartitionKeyIsString(partitionKey);
+  return getCorrectSizedPartitionKey(partitionKeyAsString);
 };
